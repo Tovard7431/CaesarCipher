8ff3a38359131cb6a93921fe9c0b70cbee808126
sam <khamovsam@gmail.com>
2025-08-20 22:52:54 +0300

    Прописал логику шифра.

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class FileManager {
+    //Отвечает за чтение и запись файлов.
+    public String readFile(String filePath) {
+        // Логика чтения файла
+        return null;
+    }
+    public void writeFile(String content, String filePath) {
+        // Логика записи файла
+        return;
+    }
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class MainApp {
+    public static void main(String[] args) {
+        byte key = 9;
+        char[] intText = new char[]{48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 33, 44, 65, 97, 97, 122};
+        System.out.println(intText);
+        System.out.println(Cipher.cipher(intText, key));
+
+
+    }
+
+
+
 }
