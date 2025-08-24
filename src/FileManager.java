14e903a5ecb89939601fbbc4418752d84532535c
sam <khamovsam@gmail.com>
2025-08-24 20:40:07 +0300

    написать класс FileManager.

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,21 +1,66 @@
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 //Отвечает за чтение и запись файлов.
 
 public class FileManager {
-    public String readFile(String filePath) {
+    public boolean readFile() {
+        byte key = 0;
+        String fileAddress = "";
+        String encryptedFileAddress = "";
+
+
         InputStream stream = System.in;
         Scanner console = new Scanner(stream);
-        System.out.println("Specify the path to the text file");
-        String fileAddress = console.nextLine();
-        System.out.println("Where should I write your ciphertext?");
-        String encryptedFileAddress = console.nextLine();
+
+        try {
+            System.out.println("Specify the path to the text file:");
+            fileAddress = console.nextLine();
+        } catch (Exception e) {
+            System.err.println("Error reading path: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("Specify the path where to write the cipher:");
+            encryptedFileAddress = console.nextLine();
+        } catch (Exception e) {
+            System.err.println("Error reading path: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("Enter the key for encoding:");
+            key = console.nextByte();
+        } catch (Exception e) {
+            System.err.println("Error reading key: " + e.getMessage());
+        }
 
 
-        return null;
-    }
-    public void writeFile(String content, String filePath) {
-        // Логика записи файла
-        return;
+        try (FileReader reader = new FileReader(fileAddress);
+             StringWriter writer = new StringWriter();
+             OutputStream in = new FileOutputStream(encryptedFileAddress)) {
+
+            char[] buffer = new char[1024];
+            int charsRead;
+
+            while ((charsRead = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, charsRead);
+            }
+
+            char[] charArray = writer.toString().toCharArray();
+            System.out.println("Characters read: " + charArray.length);
+
+            char[] buff = Cipher.cipher(charArray, key);
+            byte[] answer = new byte[Cipher.cipher(charArray, key).length];
+            for (var i = 0; i < answer.length; i++) {
+                answer[i] = (byte) buff[i];
+            }
+
+            in.write(answer);
+
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+
+        }
+        return false;
     }
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,12 +1,11 @@
-import java.io.InputStream;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
-        byte key = 9;
-        char[] intText = new char[]{48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 33, 44, 65, 97, 97, 122};
-        System.out.println(intText);
-        System.out.println(Cipher.cipher(intText, key));
+        FileManager fileManager = new FileManager();
+        fileManager.readFile();
+
     }
 }
